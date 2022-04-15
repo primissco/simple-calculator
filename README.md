2d174a0e4b0e68058204a6d7a2c9fdf55fa961f3
primissco <hello@primiss.co>
2022-04-15 20:05:16 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple calculator written in python
+You will not need to install any dependencies, this script should _"just work"_.
---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+def clear():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+firstnumber = float(input("Put in your first number:    "))
+clear()
+print(f"First number: {firstnumber}")
+secondnumber = float(input("Put in your second number:    "))
+clear()
+print(f"First number: {firstnumber}")
+print(f"Second number: {secondnumber}")
+calc = str(input("What do you want to do with them? (+,-,*,/):  "))
+clear()
+
+if calc == "+":
+    print(f"{firstnumber} + {secondnumber} = ", firstnumber + secondnumber)
+elif calc == "-":
+    print(f"{firstnumber} - {secondnumber} = ", firstnumber - secondnumber)
+elif calc == "*":
+    print(f"{firstnumber} * {secondnumber} = ", firstnumber * secondnumber)
+elif calc == "/":
+    print(f"{firstnumber} / {secondnumber} = ", firstnumber / secondnumber)
+
+input()
